ceec64e8791d0a854466b3b84d44d137f6c92104
plarika <148452619+plarika@users.noreply.github.com>
2026-09-07 00:07:17 +0100

    fix: resolve Compose weight compilation

--- app/src/main/java/app/revanced/manager/ui/screen/AppsScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/AppsScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
--- app/src/main/java/app/revanced/manager/ui/screen/DashboardScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/DashboardScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
