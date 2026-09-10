dca30fe041035f291c6ad954dc270c2dac929841
plarika <148452619+plarika@users.noreply.github.com>
2026-09-10 12:56:50 +0100

    fix: close official updates shell correctly

--- app/src/main/java/app/revanced/manager/ui/screen/UpdateScreen.kt
+++ app/src/main/java/app/revanced/manager/ui/screen/UpdateScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -154,6 +155,7 @@ fun UpdateScreen(
     }
 }
 
+}
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun MeteredDownloadConfirmationDialog(
@@ -182,5 +184,4 @@ private fun MeteredDownloadConfirmationDialog(
         icon = { Icon(Icons.Outlined.Update, null) },
         text = { Text(stringResource(R.string.download_confirmation_metered)) }
     )
-    }
 }
